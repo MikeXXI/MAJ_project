b39d8196ad3395245b75e348bb831a2f6190cbe9
Mike <mickael.djegherif@outlook.fr>
2024-03-21 10:47:54 +0100

    update dev

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,14 @@
+
+{
+typeof import("tailwindcss").Config;
+}
+
+const tailwindConfig = {
+  content: ["./src/*/.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = tailwindConfig;
