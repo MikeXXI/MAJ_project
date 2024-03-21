1cc8940cf213d340540d1a4fa2dddd54237ed3d4
Aiiishaaa <aicha.hamida06@yahoo.fr>
2024-03-21 11:02:27 +0100

    Tailwindcss added

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,10 +1,5 @@
-
-{
-typeof import("tailwindcss").Config;
-}
-
 const tailwindConfig = {
-  content: ["./src/*/.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
